afe778be8946851836321059f0553614d35f716c
fabiola <fabiolasilvaoffice@gmail.com>
2022-08-02 16:35:03 -0400

    Resolvendo o desafio do antecessor e sucessor

--- desafios/desafio3.py
+++ desafios/desafio3.py
@@ -1,1 +1,8 @@
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor  e seu antecessor. 
+
+numero = int(input('Digite um valor: '))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('O antecessor do  número {} é: {} e seu sucessor é: {}'.format(numero, antecessor, sucessor))
